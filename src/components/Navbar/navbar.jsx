7259e400f479b298cba857df75f4d21305404058
Rohit-Yadav-Official <roadtocode76448@gmail.com>
2025-05-09 14:28:53 +0530

    make navbar mobile friendly

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,26 +1,38 @@
+import { useState } from 'react';
 import rohit from '../../assets/logo.png';
 import contact from '../../assets/contact.png';
 import './navbar.css';
-import { Link  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
+import { GiHamburgerMenu } from 'react-icons/gi'; // npm install react-icons
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navBar">
       <img src={rohit} alt="logo" className="logo" />
-      <div className="desktopMenu">
-      <ScrollLink to="intro" className="desktopMenuListItem" smooth={true} duration={500}>Home</ScrollLink>
-<ScrollLink to="projects" className="desktopMenuListItem" smooth={true} duration={500}>Projects</ScrollLink>
-<ScrollLink to="dsa" className="desktopMenuListItem" smooth={true} duration={500}>DSA</ScrollLink>
-<Link to="/blog" className="desktopMenuListItem">Blog</Link>
 
+      <div className="hamburger" onClick={toggleMenu}>
+        <GiHamburgerMenu size={25} />
       </div>
-      <ScrollLink to="contact" smooth={true} duration={500}>
-        <button className="contactButton">
-          <img src={contact} alt="contact" className="contactNavBar" />
-          Contact Me
-        </button>
-      </ScrollLink>
+
+      <div className={`right section ${menuOpen ? 'open' : ''}`}>
+        <div className="desktopMenu">
+          <ScrollLink to="intro" className="desktopMenuListItem" smooth={true} duration={500}>Home</ScrollLink>
+          <ScrollLink to="projects" className="desktopMenuListItem" smooth={true} duration={500}>Projects</ScrollLink>
+          <ScrollLink to="dsa" className="desktopMenuListItem" smooth={true} duration={500}>DSA</ScrollLink>
+          <Link to="/blog" className="desktopMenuListItem">Blog</Link>
+        </div>
+        <ScrollLink to="contact" smooth={true} duration={500}>
+          <button className="contactButton">
+            <img src={contact} alt="contact" className="contactNavBar" />
+            Contact Me
+          </button>
+        </ScrollLink>
+      </div>
     </nav>
   );
 };
